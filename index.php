befc5e870804a61d10380ec3b7d2adea7caab0e4
Edith Gonzalez <edgonzalez-reynoso@csumb.edu>
2017-10-27 21:10:58 +0000

    updated index.php

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Lab 05</title>
+        <title>Shopping Cart</title>
         <style>
         </style>
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
 
     </body>
 </html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <?php
+    
+            $dbHost = getenv('IP');
+            $dbPort = 3306;
+            $dbName = "blockbuster";
+            $username = 'root';
+            $password = "";
+            
+            $dbConn = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName", $username, $password);
+            $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        
+      
+
+            echo "Movies: <br/ >";
+            $sql = "SELECT movie_title FROM `movie`";
+            		
+            		
+            $stmt = $dbConn->query($sql);	
+            $results = $stmt->fetchAll();
+            foreach ($results as $record) {
+            	echo $record['movie_title'];
+            	echo "<br />";
+            }
+?>
