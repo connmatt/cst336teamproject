e878dc3d582451da83b3ac2f60d536bd00c930c7
Eric Tinoco <ertinoco@csumb.edu>
2017-10-28 05:31:00 +0000

    blah

--- inc.form.php
+++ inc.form.php
@@ -33,38 +33,6 @@
     echo "</tbody>";
     echo "</table>";
   }
-  
-  //Get the type of format
-  function getFormats(){
-    global $dbConn;
-    $sql = "SELECT DISTINCT(format_type)
-    FROM `formats`
-    ORDER BY format_type";
-    
-    $stmt = $dbConn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    foreach ($records as $record){
-      echo "<option>" . $record['format_type'] . "</option>";
-    }
-  }
-
-  //Get the Genre
-  function getGenre(){
-    global $dbConn;
-    $sql = "SELECT DISTINCT(movie_category)
-    FROM `movie`
-    ORDER BY movie_category";
-    
-    $stmt = $dbConn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    foreach ($records as $record){
-      echo "<option>" . $record['movie_category'] . "</option>";
-    }
-  }
 
   //Determine how to display after hitting the submit button
   function submit(){
@@ -75,7 +43,9 @@
       $celeb = $_GET['celeb'];
       $format = $_GET['format'];
       $filter = $_GET['filter'];
+      $organize = $_GET['organize'];
       $temp = "";
+      $oTemp = "";
       $filterStatus = false;
       
       if(!empty($_GET['filter'])){
@@ -84,24 +54,30 @@
       else{
         $filterStatus = false;
       }
+      if($organize == "asc"){
+        $oTemp = "ASC";
+      }
+      else{
+        $oTemp = "DESC";
+      }
       
       if ($filterStatus == true){
         if ($filter == 'title'){
           $temp = "SELECT movie_title, movie_category, release_year, movie_id
                    FROM movie
-                   ORDER BY movie_title DESC";
+                   ORDER BY movie_title " . $oTemp;
           displayData($temp);
         }
         else if($filter == 'genre'){
           $temp = "SELECT movie_title, movie_category, release_year, movie_id
                    FROM movie
-                   ORDER BY movie_category DESC";
+                   ORDER BY movie_category " . $oTemp;
           displayData($temp);
         }
         else if($filter == 'year'){
           $temp = "SELECT movie_title, movie_category, release_year, movie_id
                    FROM movie
-                   ORDER BY release_year DESC";
+                   ORDER BY release_year " . $oTemp;
           displayData($temp);
         }
       }
@@ -121,30 +97,19 @@
   <div id="wrapper">
     <h1>Movie Search Engine</h1>
     <form method='get'>
-      <h3>Search through the following methods:</h3>
-      
-      Movie Title:
-      <input type="text" name="movieTitle" placeholder="Movie Title" />
-      <br /><br />
-      
-      Genre:
-      <select name="genre">
-      <option>Select Genre</option>
-      <?=getGenre()?>
-      </select>
-      <br /><br />
-      
-      Format Type:
-      <select name="format_type">
-      <option> Select Format</option>
-      <?=getFormats()?>
-      </select>
-      <br /><br />
+      <h3>Filter through the following methods:</h3>
       
       Filter by Type:
       <input type="radio" name="filter" value="title" /> Movie Title
       <input type="radio" name="filter" value="genre" /> Genre
       <input type="radio" name="filter" value="year" /> Year Released
+      <br>
+      
+      Organize By:
+      <select name="organize">
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+      </select>
       
       <input type="submit" value="Go" name="submit" />
     </form>
